7cc90e56a422b03078705de9beaa20bcecdbaa6e
Daisuke Morita <daisuke.marubinotto@gmail.com>
2019-05-09 18:10:04 +0900

    Install and test whatwg-fetch

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,5 @@
+import 'whatwg-fetch'
+
 let changeColor = document.getElementById('changeColor');
 
 chrome.storage.sync.get('color', function (data) {
@@ -6,6 +8,25 @@ chrome.storage.sync.get('color', function (data) {
 });
 
 changeColor.onclick = function (element) {
+  fetch('http://localhost:4000/api/cotos', {
+    credentials: 'include',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest'
+    },
+    body: JSON.stringify({
+      coto: {
+        content: "Hello from Cotoami Scraper!",
+        summary: null,
+        cotonoma_id: null
+      }
+    })
+  })
+    .then((data) => data.json())
+    .then((data) => console.log('data', data))
+
+
   let color = element.target.value;
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.executeScript(

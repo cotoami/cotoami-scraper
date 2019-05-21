64c8efdd5087b31d46a8efe945c81c11cad3fb3b
Daisuke Morita <daisuke.marubinotto@gmail.com>
2019-05-22 00:42:34 +0900

    Display cotoami session

--- src/Popup.vue
+++ src/Popup.vue
@@ -1,30 +1,46 @@
 <template>
   <div id="popup">
-    <div id="posted" v-if="posted">
-      <img src="./done.gif"> Posted.
-    </div>
-    <div id="scraped" v-else-if="scraper && scraper.scraped">
-      <div id="scraped-content" v-html="scraper.html()"></div>
-      <div class="buttons">
-        <button class="button" v-on:click="cancel()" v-if="!posting">Cancel</button>
-        <button
-          class="button button-primary"
-          v-on:click="post()"
-          v-bind:disabled="posting"
-        >{{ posting ? 'Posting...' : 'Post' }}</button>
+    <div id="in-session" v-if="session">
+      <div id="posted" v-if="posted">
+        <img src="./done.gif"> Posted.
+      </div>
+      <div v-else>
+        <div v-if="scraper">
+          <div id="scraped" v-if="scraper.scraped">
+            <div id="scraped-content" v-html="scraper.html()"></div>
+            <div class="buttons">
+              <button class="button" v-on:click="cancel()" v-if="!posting">Cancel</button>
+              <button
+                class="button button-primary"
+                v-on:click="post()"
+                v-bind:disabled="posting"
+              >{{ posting ? 'Posting...' : 'Post' }}</button>
+            </div>
+          </div>
+          <div id="scaping" v-else-if="scraper.scaping">Scaping...</div>
+        </div>
+        <div id="main-menu" v-else>
+          <div id="amishi">
+            <img class="avatar" v-bind:src="session.amishi.avatar_url">
+            <span class="name">{{session.amishi.display_name}}</span>
+          </div>
+          <div id="scrape-buttons">
+            <div id="scrape-title" v-on:click="scrape(createPageLinkScraper())">
+              <button class="button">Page link</button>
+            </div>
+            <div id="scrape-selection">
+              <button class="button" v-bind:disabled="textSelected">Selection</button>
+            </div>
+            <div id="scrape-kindle-highlights">
+              <button class="button" disabled>Kindle highlights</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div id="scaping" v-else-if="scraper && scraper.scaping">Scaping...</div>
-    <div id="scrape-buttons" v-else>
-      <div id="scrape-title" v-on:click="scrape(createPageLinkScraper())">
-        <button class="button">Page link</button>
-      </div>
-      <div id="scrape-selection">
-        <button class="button" v-bind:disabled="textSelected">Selection</button>
-      </div>
-      <div id="scrape-kindle-highlights">
-        <button class="button" disabled>Kindle highlights</button>
-      </div>
+    <div id="no-session" v-else>
+      You need to sign in to:
+      <a v-bind:href="cotoamiUrl" target="_blank">{{cotoamiUrl}}</a>
     </div>
   </div>
 </template>
@@ -36,6 +52,8 @@ import PageLinkScraper from "./PageLinkScraper.js";
 export default {
   data() {
     return {
+      cotoamiUrl: COTOAMI_URL,
+      session: null,
       scraper: null,
       textSelected: false,
       posting: false,
@@ -44,6 +62,28 @@ export default {
   },
 
   methods: {
+    fetchSession() {
+      fetch(COTOAMI_URL + "/api/public/session", {
+        credentials: "include"
+      })
+        .then(response => {
+          if (response.status >= 200 && response.status < 300) {
+            return response.json();
+          } else {
+            var error = new Error(response.statusText);
+            error.response = response;
+            throw error;
+          }
+        })
+        .then(json => {
+          console.log("session", json);
+          this.session = json;
+        })
+        .catch(error => {
+          console.log("request failed", error);
+        });
+    },
+
     checkSelection() {
       chrome.tabs.executeScript(
         {
@@ -93,6 +133,7 @@ export default {
   },
 
   beforeMount() {
+    this.fetchSession();
     this.checkSelection();
   }
 };

73f42d30be787ed6f96a2c24ecb82870cc720cc4
Daisuke Morita <daisuke.marubinotto@gmail.com>
2019-05-22 10:21:54 +0900

    Add PageLinkScraper.vue

---
+++ src/PageLinkScraper.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="page-link-scraper">
+    <div id="posted" v-if="posted">
+      <img src="./images/done.gif"> Posted.
+    </div>
+    <div id="scraped" v-else-if="scraped">
+      <div id="scraped-content">
+        <div class="coto">
+          <a v-bind:href="url" target="_blank">{{ title }}</a>
+        </div>
+      </div>
+      <div class="buttons">
+        <button class="button" v-on:click="cancel()" v-if="!posting">Cancel</button>
+        <button
+          class="button button-primary"
+          v-on:click="post()"
+          v-bind:disabled="posting"
+        >{{ posting ? 'Posting...' : 'Post' }}</button>
+      </div>
+    </div>
+    <div id="scaping" v-else-if="scaping">Scaping...</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+      url: "",
+      scaping: false,
+      scraped: false,
+      posting: false,
+      posted: false
+    };
+  },
+
+  methods: {
+    scrape() {
+      this.scaping = true;
+      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        this.title = tabs[0].title;
+        this.url = tabs[0].url;
+        this.scraped = true;
+        this.scaping = false;
+      });
+    },
+
+    cancel() {
+      this.$emit("cancel");
+    },
+
+    markdown() {
+      return `[${this.title}](${this.url})`;
+    },
+
+    post() {
+      this.posting = true;
+      fetch(COTOAMI_URL + "/api/cotos", {
+        credentials: "include",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Requested-With": "XMLHttpRequest",
+          "X-Cotoami-Client-Id": "dummy"
+        },
+        body: JSON.stringify({
+          coto: {
+            content: this.markdown(),
+            summary: null,
+            cotonoma_id: null
+          }
+        })
+      }).then(data => {
+        this.posting = false;
+        this.posted = true;
+      });
+    }
+  },
+
+  beforeMount() {
+    this.scrape();
+  }
+};
+</script>
--- src/Popup.vue
+++ src/Popup.vue
@@ -1,39 +1,23 @@
 <template>
   <div id="popup">
     <div id="in-session" v-if="session">
-      <div id="posted" v-if="posted">
-        <img src="./images/done.gif"> Posted.
+      <div v-if="scraper">
+        <component v-bind:is="scraper" v-on:cancel="closeScraper"></component>
       </div>
-      <div v-else>
-        <div v-if="scraper">
-          <div id="scraped" v-if="scraper.scraped">
-            <div id="scraped-content" v-html="scraper.html()"></div>
-            <div class="buttons">
-              <button class="button" v-on:click="cancel()" v-if="!posting">Cancel</button>
-              <button
-                class="button button-primary"
-                v-on:click="post()"
-                v-bind:disabled="posting"
-              >{{ posting ? 'Posting...' : 'Post' }}</button>
-            </div>
-          </div>
-          <div id="scaping" v-else-if="scraper.scaping">Scaping...</div>
+      <div id="main-menu" v-else>
+        <div id="amishi">
+          <img class="avatar" v-bind:src="session.amishi.avatar_url">
+          <span class="name">{{session.amishi.display_name}}</span>
         </div>
-        <div id="main-menu" v-else>
-          <div id="amishi">
-            <img class="avatar" v-bind:src="session.amishi.avatar_url">
-            <span class="name">{{session.amishi.display_name}}</span>
+        <div id="scrape-buttons">
+          <div id="scrape-title" v-on:click="scraper = 'page-link-scraper'">
+            <button class="button">Page link</button>
           </div>
-          <div id="scrape-buttons">
-            <div id="scrape-title" v-on:click="scrape(createPageLinkScraper())">
-              <button class="button">Page link</button>
-            </div>
-            <div id="scrape-selection">
-              <button class="button" v-bind:disabled="textSelected">Selection</button>
-            </div>
-            <div id="scrape-kindle-highlights">
-              <button class="button" disabled>Kindle highlights</button>
-            </div>
+          <div id="scrape-selection">
+            <button class="button" v-bind:disabled="textSelected">Selection</button>
+          </div>
+          <div id="scrape-kindle-highlights">
+            <button class="button" disabled>Kindle highlights</button>
           </div>
         </div>
       </div>
@@ -47,7 +31,7 @@
 
 <script>
 import "whatwg-fetch";
-import PageLinkScraper from "./PageLinkScraper.js";
+import PageLinkScraper from "./PageLinkScraper.vue";
 
 export default {
   data() {
@@ -55,10 +39,12 @@ export default {
       cotoamiUrl: COTOAMI_URL,
       session: null,
       scraper: null,
-      textSelected: false,
-      posting: false,
-      posted: false
+      textSelected: false
     };
+  },
+
+  components: {
+    "page-link-scraper": PageLinkScraper
   },
 
   methods: {
@@ -95,40 +81,8 @@ export default {
       );
     },
 
-    createPageLinkScraper() {
-      return new PageLinkScraper();
-    },
-
-    scrape(scraper) {
-      this.scraper = scraper;
-      this.scraper.scrape();
-    },
-
-    cancel() {
+    closeScraper() {
       this.scraper = null;
-    },
-
-    post() {
-      this.posting = true;
-      fetch(COTOAMI_URL + "/api/cotos", {
-        credentials: "include",
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "X-Requested-With": "XMLHttpRequest",
-          "X-Cotoami-Client-Id": "dummy"
-        },
-        body: JSON.stringify({
-          coto: {
-            content: this.scraper.markdown(),
-            summary: null,
-            cotonoma_id: null
-          }
-        })
-      }).then(data => {
-        this.posting = false;
-        this.posted = true;
-      });
     }
   },
 

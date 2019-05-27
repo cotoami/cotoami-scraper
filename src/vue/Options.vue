29b1eeb4a1a9aa9ee266501dd6ef7c33714b89a5
Daisuke Morita <daisuke.marubinotto@gmail.com>
2019-05-27 12:13:31 +0900

    Options.vue: #options-form

--- src/vue/Options.vue
+++ src/vue/Options.vue
@@ -1,12 +1,33 @@
 <template>
-  <div id="options">Hello</div>
+  <div id="options">
+    <div id="content">
+      <div id="title">Cotoami Scraper Options</div>
+      <form id="options-form">
+        <div class="row">
+          <label for="cotoami-url">Cotoami URL</label>
+          <input
+            class="u-full-width"
+            type="text"
+            placeholder="Cotoami URL"
+            id="cotoami-url"
+            v-model="cotoamiUrlInput"
+          >
+        </div>
+        <div id="buttons" class="row">
+          <button class="button" v-on:click="reset()" v-if="!posting">Reset</button>
+          <button class="button button-primary" v-bind:disabled="!okToSave()">Save</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      cotoamiUrl: ""
+      cotoamiUrl: "",
+      cotoamiUrlInput: ""
     };
   },
 
@@ -14,7 +35,19 @@ export default {
     init() {
       chrome.storage.sync.get("cotoamiUrl", data => {
         this.cotoamiUrl = data.cotoamiUrl;
+        this.cotoamiUrlInput = data.cotoamiUrl;
       });
+    },
+
+    reset() {
+      this.cotoamiUrlInput = this.cotoamiUrl;
+    },
+
+    okToSave() {
+      return (
+        this.cotoamiUrlInput.trim() !== "" &&
+        this.cotoamiUrlInput.trim() != this.cotoamiUrl
+      );
     }
   },
 

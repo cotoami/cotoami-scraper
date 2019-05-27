c1d27628e718335f5f1526635ba266f4b85ea2ca
Daisuke Morita <daisuke.marubinotto@gmail.com>
2019-05-27 11:30:50 +0900

    Add src/vue/Options.vue

--- src/options.js
+++ src/options.js
@@ -1,15 +1,6 @@
-let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
-    let button = document.createElement('button');
-    button.style.backgroundColor = item;
-    button.addEventListener('click', function () {
-      chrome.storage.sync.set({ color: item }, function () {
-        console.log('color is ' + item);
-      })
-    });
-    page.appendChild(button);
-  }
-}
-constructOptions(kButtonColors);
+import Vue from 'vue';
+import Options from './vue/Options.vue';
+import "./css/skeleton/skeleton.scss";
+import "./css/options.scss";
+
+new Vue({ el: '#options', render: c => c(Options) });
---
+++ src/vue/Options.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="options">Hello</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cotoamiUrl: ""
+    };
+  },
+
+  methods: {
+    init() {
+      chrome.storage.sync.get("cotoamiUrl", data => {
+        this.cotoamiUrl = data.cotoamiUrl;
+      });
+    }
+  },
+
+  beforeMount() {
+    this.init();
+  }
+};
+</script>
